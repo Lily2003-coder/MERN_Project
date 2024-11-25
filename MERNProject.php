f647b57708d905a65fcdd2aeb577da6be1f007aa
lily2003-coder <lillygeorge0225@gmail.com>
2024-11-25 15:38:46 +0530

    first commit

---
+++ MERNProject.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color:blueviolet;
+            border: 2px dotted black;
+          /*display: inline-block;*/
+            border-style: dotted;
+        }
+    </style>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Welcome to <span id="highlight">HTML</span> Session</h1>
+
+    <header><nav><ul>
+        <li><a href="#" >Home</a></li>
+        <li><a href="#" >About</a></li>
+        <li><a href="#" >Contact</a></li>
+       
+
+    </ul></nav></header>
+   <main><p style="font-size:20px;" class="para-style;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+    Sequi ut debitis, in fugiat commodi numquam dolores perferendis quidem obcaecati architecto. 
+    Corporis iusto, libero quibusdam perferendis ullam laborum laudantium quidem tempore accusantium
+    incidunt, quas similique illum mollitia quaerat ipsa. Mollitia at eos, esse officiis accusantium
+    sint id, dolor tempore totam alias amet possimus commodi laborum earum corrupti. 
+    Atque architecto at id optio possimus impedit doloremque aliquam! Unde, atque. 
+    Expedita culpa doloribus, similique autem debitis earum recusandae nesciunt voluptatem officiis porro, 
+    hic voluptatibus ipsum amet rerum eveniet suscipit quos vel. Incidunt expedita fugit placeat,
+    ducimus iusto nostrum et odio assumenda illum veritatis.</p></main>
+
+    <a href="https://www.google.com/" target="_blank">Visit Google</a><br><br>
+
+    <img class="mypic1" src="http://localhost/ProjectFnal/mernpic1.jpeg" alt="Butterfly"/>
+    <figcaption>This is my world</figcaption>
+
+    <img class="mypic2" src="https://wallpapercave.com/wp/CcV4UhO.jpg" alt="myfriends"/>
+    <figcaption>My Friends</figcaption>
+
+</body>
+</html>
+
